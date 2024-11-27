48bfd0cf0769c12823cf7b752cac97d7b4a9308b
xiaoen-217 <Kimi950217@yahoo.com.tw>
2024-11-27 06:30:40 +0000

    import data and rephrase

--- 2.R
+++ 2.R
@@ -1,3 +1,13 @@
 library(tidyverse)
-gs <- read_csv("gp_p_01.csv")
-glimpse(gs)
+budget <-  read_csv("102年度新北市總預算歲入來源別預算比較總表（經資併計）_export.csv")
+glimpse(budget)
+budget <- budget %>%
+  rename(
+    項目 = field1,
+    本年度預算數 = field2,
+    上年度預算數 = field3,
+    前年度決算數 = field4,
+    本年度與上年度比較 = field5
+  )
+
+
